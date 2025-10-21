75e4c9c9a5b2f8d94092df2d94a3ab5000b856d3
2cytechie <3302636082@qq.com>
2025-10-22 00:42:09 +0800

    员工分页查询

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -1,10 +1,17 @@
 package com.sky.config;
 
 import com.sky.interceptor.JwtTokenAdminInterceptor;
+import com.sky.json.JacksonObjectMapper;
+
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -66,5 +73,18 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         log.info("start__开始注册静态资源映射...");
         registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    /**
+     * 扩展spring MVC 框架的消息转换器
+     * @param converters
+     */ 
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // 创建一个新的消息转换器
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        // 设置消息转换器的支持媒体类型
+        converter.setObjectMapper(new JacksonObjectMapper());
+        // 将新的消息转换器添加到转换器列表中
+        converters.add(0, converter);
     }
 }
